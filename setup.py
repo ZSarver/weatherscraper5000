ee507bd36a7a0897912743b17c1510aec4a123e2
Zachary Sarver <Zachary.Sarver@gmail.com>
2017-06-15 22:42:01 -0500

    Setup fixes and improvements
    Excluded packaging the tests and added install requirements for
    dependency resolution

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="WeatherScraper5000",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test_*"]),
     description="It scrapes the weather better than WeatherScraper4000",
     author="Zachary Sarver",
     author_email="Zachary.Sarver@gmail.com",
     license="GNU GPL v3",
     entry_points={
         "console_scripts": ["whistory = weatherscraper5000.main"]
-    }
+    },
+    install_requires=["beautifulsoup4>=4.6.0", "requests>=2.18.1"]
 )
